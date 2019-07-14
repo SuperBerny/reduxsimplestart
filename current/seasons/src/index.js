987e8abef5bbbd42432485aea6c2f063c9e5bf20
Brian Patrick <batpat772@gmail.com>
2019-07-13 17:20:22 -0700

    more lectures done

--- current/seasons/src/index.js
+++ current/seasons/src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+   
+   constructor(props) {
+      super(props); 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      //Now the constructor function's only job is to create a state object.
+      // This will be refactored in the next lecture
+      this.state = { lat: null, errorMessage: '' };      
+   }
+
+   componentDidMount() {
+      window.navigator.geolocation.getCurrentPosition(
+         position => this.setState({ lat: position.coords.latitude }),
+         err => this.setState({ errorMessage: err.message })
+      );
+   }
+
+      //React requires that you define the render method
+   render() {
+      if (this.state.errorMessage && !this.state.latitude) {
+         return <div>Error: {this.state.errorMessage}</div>
+      }
+
+      if (!this.state.errorMessage && this.state.lat) {
+         return <div>Lattitude: {this.state.lat}</div>
+      }
+
+      return <div>Loading!</div>;
+   }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
